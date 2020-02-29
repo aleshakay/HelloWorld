efa6cb1bbe714bb91d7775b2615a6d9c4deb7484
Alesha Reed <aleshak83@gmail.com>
2020-02-29 12:21:16 -0600

    Exercise1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,8 +6,47 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
+
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Please enter a choose a dialect option? Southern, Northern, or Pirate");
+                String dialect = Console.ReadLine();
+                switch (dialect)
+                {
+                    case "Northern":
+                        Console.WriteLine("Northern");
+                        break;
+                    case "Pirate":
+                        Console.WriteLine("Pirate");
+                        break;
+                    default:
+                        Console.WriteLine("Southern");
+                        break;
+                }
+
+            }
+
             Console.WriteLine("Howdy Y'all!!");
-            Console.ReadKey(); // What happens if you run the app without this line?
+
+            var animals = new string[] { "Triceratops", "Gorilla", "Corgi", "Toucan", "cat", "dog" };
+                char[] vowels = { 'a', 'e', 'i', 'o', 'u', 'y' };
+                foreach( var animal in animals)
+                {
+                int numVowels = 0;
+                foreach (var vowel in vowels)
+                    {
+                        if (animal.Contains(vowel))
+                        {
+                            numVowels++;
+                        }
+                    }
+                    if(numVowels > 2)
+                {
+                    Console.WriteLine(animal);
+                }
+
+                }
+              
         }
     }
 }
